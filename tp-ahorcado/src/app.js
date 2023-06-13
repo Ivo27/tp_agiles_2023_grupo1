fb004f3f2b1ca05bf18b030d66e0edb994dbc56b
atomotechnology <atomotechnology@gmail.com>
2023-06-12 22:16:20 -0300

    sprint 4

--- tp-ahorcado/src/app.js
+++ tp-ahorcado/src/app.js
@@ -1,29 +1,36 @@
 const PALABRA_A_ADIVINAR = 'javascript'
 let palabraUsuario = []
-const  MAX_INTENTOS = 6
+const MAX_INTENTOS = 6
 let intentos = MAX_INTENTOS
 
 
 const verificarLetra = (letra, palabraAdivinar) => {
-   
-    if (letra.length !== 1) return false   
+
+    if (letra.length !== 1) return false
     palabraAdivinar = palabraAdivinar.toLowerCase()
-    return palabraAdivinar.includes(letra.toLowerCase()) 
+    return palabraAdivinar.includes(letra.toLowerCase())
 }
 
-const actualizarIntentos = int  => int === 0 ? 0 : --int
+const actualizarIntentos = int => int === 0 ? 0 : --int
 
-const completarPalabra = (palabraAdivinar,letra,palabraUsuario) => {
-    palabraAdivinar.split('').map((l,index) => {
-        if(l == letra) palabraUsuario[index] = letra        
+// function actualizarIntentos(int){
+//     if()
+
+//     return 
+
+// }
+
+const completarPalabra = (palabraAdivinar, letra, palabraUsuario) => {
+    palabraAdivinar.split('').map((l, index) => {
+        if (l == letra) palabraUsuario[index] = letra
     })
     return palabraUsuario
 }
 
-const verificarFinJuego = (palabraUsuario, palabraAdivinar,int) => {
-    if (palabraUsuario === palabraAdivinar)        return 'Ganaste' 
-    else{
-        if(int === 0) return 'Perdiste'        
+const verificarFinJuego = (palabraUsuario, palabraAdivinar, int) => {
+    if (palabraUsuario === palabraAdivinar) return 'Ganaste'
+    else {
+        if (int === 0) return 'Perdiste'
         else return 'Segui participando te falta ' + int + ' intentos'
     }
 }
@@ -32,10 +39,10 @@ const verificarFinJuego = (palabraUsuario, palabraAdivinar,int) => {
     devuelve false si la letra esta en la palabra a adivinar
     y la lista de las letras erradas actualizada si no esta en la palabra a adivinar
 */
-const palabraErradas = (palabraAdivinar,letra,letrasErradas) => {
-    if( verificarLetra(letra, palabraAdivinar) ){
+const palabraErradas = (palabraAdivinar, letra, letrasErradas) => {
+    if (verificarLetra(letra, palabraAdivinar)) {
         return false
-    }else{
+    } else {
         letrasErradas.push(letra)
         return letrasErradas
     }
@@ -52,12 +59,48 @@ const palabraErradas = (palabraAdivinar,letra,letrasErradas) => {
 // verificarLetra('k', PALABRA_A_ADIVINAR)
 // console.log(verificarFinJuego())
 
-// const juego = (letra) => {
-//     let existe = verificarLetra(letra, PALABRA_A_ADIVINAR)
-//     if(!existe)  actualizarIntentos()
-//     else completarPalabra(letra)
-//     console.log(verificarFinJuego())
-// }
+const jugar = (palabraAdivinar) => {
+
+    let letrasErradas = []
+    let int = 20
+    let palabraUsuario = []
+    // a variable to store all the letter of the alphabet
+    let alphabet = 'abcdefghijklmnopqrstuvwxyz';
+
+    for (let i = 0; i < 30; i++) {
+        //  get alet letter random letter from the alphabet
+        let letter = ''
+        do {
+            letter = alphabet[Math.floor(Math.random() * alphabet.length)];
+        } while (letrasErradas.includes(letter))
+
+        let existe = verificarLetra(letter, palabraAdivinar)
+        if (!existe) {
+            int = actualizarIntentos(int)
+            letrasErradas = palabraErradas(palabraAdivinar, letter, letrasErradas)
+        }
+        else completarPalabra(palabraAdivinar, letter, palabraUsuario)
+        let res = verificarFinJuego(palabraUsuario.join(''), palabraAdivinar, int)
+        console.log('Palabra a adivinar: ' + palabraUsuario.join(''), 'Letras erradas: ' + letrasErradas.join(''), 'Intentos: ' + int)
+        if (res === 'Ganaste') return 'Ganaste'
+        else if (res === 'Perdiste') return 'Perdiste'
+    }
+}
 
 
-module.exports = {  verificarLetra, actualizarIntentos,verificarFinJuego,completarPalabra,palabraErradas }
+// console.log('RESULTADO :: ------------------------------------------------------------------------------', jugar(PALABRA_A_ADIVINAR))
+// console.log('RESULTADO :: ------------------------------------------------------------------------------', jugar('f'))
+// console.log('RESULTADO :: ------------------------------------------------------------------------------', jugar('j'))
+// console.log('RESULTADO :: ------------------------------------------------------------------------------', jugar('cr'))
+// console.log('RESULTADO :: ------------------------------------------------------------------------------', jugar('htp'))
+// console.log('RESULTADO :: ------------------------------------------------------------------------------', jugar('p'))
+// console.log('RESULTADO :: ------------------------------------------------------------------------------', jugar('s'))
+// console.log('RESULTADO :: ------------------------------------------------------------------------------', jugar('x'))
+// console.log('RESULTADO :: ------------------------------------------------------------------------------', jugar('ar'))
+// console.log('RESULTADO :: ------------------------------------------------------------------------------', jugar('l'))
+// console.log('RESULTADO :: ------------------------------------------------------------------------------', jugar('g'))
+// console.log('RESULTADO :: ------------------------------------------------------------------------------', jugar('n'))
+// console.log('RESULTADO :: ------------------------------------------------------------------------------', jugar('k'))
+
+
+module.exports = { verificarLetra, actualizarIntentos, verificarFinJuego, completarPalabra, palabraErradas, jugar }
--- tp-ahorcado/src/tests/app.test.js
+++ tp-ahorcado/src/tests/app.test.js
@@ -1,4 +1,4 @@
-const { verificarLetra, actualizarIntentos, completarPalabra, verificarFinJuego, palabraErradas } = require('../app.js');
+const { verificarLetra, actualizarIntentos, completarPalabra, verificarFinJuego, palabraErradas, jugar } = require('../app.js');
 
 test('Verificar si una letra esta en la palabra', () => {
     expect(verificarLetra('a', 'javascript')).toBeTruthy();
@@ -18,33 +18,44 @@ test('Verificar los intentos en cero', () => {
     expect(actualizarIntentos(0)).toBe(0)
 });
 test('Completar una letra ', () => {
-    expect(completarPalabra('javascript', 'j' , [])).toStrictEqual(['j'])
+    expect(completarPalabra('javascript', 'j', [])).toStrictEqual(['j'])
 });
 test('Completar varias letras ', () => {
-    expect(completarPalabra('javascript', 'a' , [])).toStrictEqual([ , 'a',,'a'])
+    expect(completarPalabra('javascript', 'a', [])).toStrictEqual([, 'a', , 'a'])
 });
 test('No completar letra', () => {
-    expect(completarPalabra('javascript', 'q' , [])).toStrictEqual([])
+    expect(completarPalabra('javascript', 'q', [])).toStrictEqual([])
 });
 
-
 test('Validar si la letra erronea se agrego ', () => {
-    expect(palabraErradas('javascript', 'q' , [])).toStrictEqual(['q'])
+    expect(palabraErradas('javascript', 'q', [])).toStrictEqual(['q'])
 });
 
 test('Validar si la letra correcta no se agrega ', () => {
-    expect(palabraErradas('javascript', 't' , [])).toBeFalsy()
+    expect(palabraErradas('javascript', 't', [])).toBeFalsy()
 });
 
 test('Validar si la letra erronea se agrego a la lista previa ', () => {
-    expect(palabraErradas('javascript', 'q' , ['o'])).toStrictEqual(['o','q'])
+    expect(palabraErradas('javascript', 'q', ['o'])).toStrictEqual(['o', 'q'])
 });
-
 
 test('Fin de juego con ganador', () => {
-    expect(verificarFinJuego('javascript', 'javascript' , 2)).toStrictEqual('Ganaste')
+    expect(verificarFinJuego('javascript', 'javascript', 2)).toStrictEqual('Ganaste')
 });
 test('Fin de juego sin ganador', () => {
-    expect(verificarFinJuego('javascript', 'javascrit' , 0)).toStrictEqual('Perdiste')
+    expect(verificarFinJuego('javascript', 'javascrit', 0)).toStrictEqual('Perdiste')
+});
+
+// dudas
+
+test('Juego aleatorio', () => {
+    let r = jugar('bar')
+    if (r === 'Ganaste') expect(r).toStrictEqual('Ganaste')
+    else expect(r).toStrictEqual('Perdiste')
+});
+test('Juego aleatorio', () => {
+    let r = jugar('j')
+    if (r === 'Ganaste') expect(r).toStrictEqual('Ganaste')
+    else expect(r).toStrictEqual('Perdiste')
 });
 
